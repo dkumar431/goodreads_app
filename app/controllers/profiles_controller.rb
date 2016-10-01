76e8101f103f8de65e18a1353772b8aa70764d14
Deepak <dkumar431@gmail.com>
2016-10-01 07:41:25 +0530

    Testing

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -11,16 +11,16 @@ class ProfilesController < ApplicationController
   end
 
   def show
-    @user = User.find_by_goodreads_user_id(params[:id])
-    
-    if @user.blank? 
+    user = User.find_by_goodreads_user_id(params[:id])
+    if user.blank? 
       response = RestClient.get(GREADS[:get_user_url],params: goodreads_params(params[:id]))
       user_details = Hash.from_xml(response)
-      @new_user = User.new(user_params(user_details))
-      @new_user.save
-      @user = @new_user
+      new_user = User.new(user_params(user_details))
+      new_user.save
+      user = new_user
     end
-    @user_p = UserPresenter.new(@user,view_context)
+    @user_p = UserPresenter.new(user,view_context)
+    
   end
   
   def my_books
