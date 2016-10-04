dd1eee7d0e01071530f8796b3e9f0a05ff8ad22d
Deepak <dkumar431@gmail.com>
2016-10-04 10:24:54 +0530

    Added books structure

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,30 +1,29 @@
 class BooksController < ApplicationController
   def owned
     book_details = goodreads_client.owned_books(goodreads_client.user_id)
-    binding.pry
-    book = Book.new(books_params(book_details))
+    books_params(book_details).each do |book|
+      b = Book.new(book)
+      b.save
+    end
   end
 
   private
   def books_params(book_details)
-    #   book_api_path = 'book_details["owned_books"]["owned_book"]'
-    #   books_arr = []
-    #   book_details["owned_books"]["owned_book"].each do |b|
-    #     book = {
-    #         goodreads_book_id: b["book"]["id"] ,
-    #         title: b["book"]["title"] ,
-    #         description: b["book"]["description"] ,
-    #         publisher: b["book"]["publisher"] ,
-    #         link: b["book"]["link"] ,
-    #         image_url: b["book"]["image_url"] ,
-    #         rating: b["book"]["average_rating"]
-    #     }
-    #     books_arr.each do |nb|
-    #         binding.pry
-    #         books_arr << book unless b["book"]["id"] == nb["book"]["id"]
-    #     end
-        
-    #   end
+      book_api_path = 'book_details["owned_books"]["owned_book"]'
+      books_arr = []
+      book_details["owned_books"]["owned_book"].each do |b|
+        book = {
+            goodreads_book_id: b["book"]["id"] ,
+            title: b["book"]["title"] ,
+            description: b["book"]["description"] ,
+            publisher: b["book"]["publisher"] ,
+            link: b["book"]["link"] ,
+            image_url: b["book"]["image_url"] ,
+            rating: b["book"]["average_rating"]
+        }
+        books_arr << book 
+      end
+      books_arr.uniq! {|ele| ele[:goodreads_book_id]}
   end
 
 end
